e49a7a702838f4ec671551f8083f2ab332d68e7c
Gustavo <53586541+gpohren@users.noreply.github.com>
2023-08-20 21:20:34 -0300

    update ami

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -9,8 +9,8 @@ variable "aws_profile" {
 }
 
 variable "ami" {
-  description = "Ubuntu 20.04"
-  default     = "ami-0c4f7023847b90238"
+  description = "Ubuntu, 20.04 LTS"
+  default     = "ami-0261755bbcb8c4a84"
 }
 
 variable "instance_micro" {
